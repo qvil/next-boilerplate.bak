ad30bd01c09a2e6bbf4ffc5e8a4de34e34c6cb52
현태수 <qvil@op.gg>
2019-09-21 15:04:48 +0900

    Merge feature/typescript

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 
+interface ButtonProps {
+  children?: any;
+  disabled?: boolean;
+}
+
 const StyledButton = styled.button`
   padding: 8px;
   background: ${({ disabled }) => (disabled ? "gray" : "papayawhip")};
   color: pink;
 `;
 
-const Button = ({ disabled, children }) => {
+const Button: React.FC<ButtonProps> = ({ disabled, children }) => {
   return <StyledButton disabled={disabled}>{children}</StyledButton>;
 };
 
