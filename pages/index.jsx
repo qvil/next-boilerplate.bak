100ac91ca9befe16d45a3167d6a6ee8f072b2a31
Taesu Hyeon <qvil@op.gg>
2019-09-23 17:37:07 +0900

    Cookie with nookies(Client/Server)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { parseCookies, setCookie } from "nookies";
+
 const Home = () => {
-  return <div>Hello World</div>;
+  const [input, setInput] = useState("");
+  const { myCookie } = parseCookies();
+  console.log("Client cookie -> myCookie", myCookie);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Input cookie"
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
+      <button onClick={() => setCookie(undefined, "myCookie", input)}>
+        setCookie
+      </button>
+    </div>
+  );
+};
+
+Home.getInitialProps = async ctx => {
+  const { myCookie } = parseCookies(ctx);
+  console.log("Server cookie -> myCookie", myCookie);
+
+  return { myCookie };
 };
 
 export default Home;
